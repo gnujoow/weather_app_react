358d7312ee8f6f8d91b4fffb076cfb717d52a8b6
KIMWOOJUNG <gnujoow.kim@gmail.com>
2016-08-06 23:19:36 +0900

    react form element
    in spa app we dont want refresh browers when it submitted

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -10,22 +10,27 @@ export default class SearchBar extends React.Component {
   }
 
   onInputChange(event) {
-    console.log(event.target.value);
     this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    // we need to go and fetch weather data
   }
 
   render() {
     return (
-      <from className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="여러분 도시의 날씨를 알아봐요"
           className="form-control"
           value={this.state.term}
           onChange={this.onInputChange} />
         <span className='input-group-btn'>
-          <button className="btn btn-primary">고고해</button>
+          <button type="submit" className="btn btn-primary">고고해</button>
         </span>
-      </from>
+      </form>
     );
   }
 }
