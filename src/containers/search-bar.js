d6c5a1f630d0561a5dee5a6527c62e89a4a6a9e7
KIMWOOJUNG <gnujoow.kim@gmail.com>
2016-08-06 22:34:10 +0900

    component added search-bar

---
+++ src/containers/search-bar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class SearchBar extends React.Component {
+  render() {
+    return (
+      <from className="input-group">
+        <input />
+        <span className='input-group-btn'>
+          <button className="btn btn-primary">고고해</button>
+        </span>
+      </from>
+    );
+  }
+}
