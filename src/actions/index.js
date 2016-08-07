e118da85378ad74169c7156f3355d4d0b2ecffef
KIMWOOJUNG <gnujoow.kim@gmail.com>
2016-08-07 15:29:57 +0900

    axios reposn by onclicking

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
-import API_KEY from '../apikey';
+import axios from 'axios';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+const API_KEY = '071a2e54087c92c90ca11467fa44e6ea';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+
+export function fetchWeather(city) {
+  console.log(API_KEY);
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
